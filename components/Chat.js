ccd3e1acb43ac502146294505fbcfbcc31c90db7
jakeportaro <jakeportaro@gmail.com>
2023-01-18 11:27:44 -0700

    added saved message function for chat. 5.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
@@ -10,41 +13,89 @@ export default class Chat extends React.Component {
     this.state = {
       backgroundColor: this.props.route.params.color,
       messages: [],
-    };
-  }
 
-  componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 2,
-          text: "You've entered the chat",
-          createdAt: new Date(),
-          system: true,
-         },
-      ],
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyA-2FxHetIu2-UbQ2X2mXXnREKcac2fF7A",
+        authDomain: "test-e14ab.firebaseapp.com",
+        projectId: "test-e14ab",
+        storageBucket: "test-e14ab.appspot.com",
+        messagingSenderId: "371306460083",
+        appId: "1:371306460083:web:0e161c459cb5e71a5cd1c6",
+      });
+    }
+    this.referenceMessagesUser = null;
+    this.addMessage = this.addMessage.bind(this);
+  }
+  onCollectionUpdate = (querySnapshot) => {
+   const messages = [];
+   // go through each document
+   querySnapshot.forEach((doc) => {
+     // get the QueryDocumentSnapshot's data
+     let data = doc.data();
+     messages.push({
+       _id: data._id,
+       text: data.text,
+       createdAt: data.createdAt.toDate(),
+       user: data.user,
+     });
+   });
+   this.setState({ messages });
+  };
+
+   // save message to Firestore
+   addMessage = () => {
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text || "",
+      createdAt: message.createdAt,
+      user: message.user,
     });
+  };
+  componentDidMount() {
+     // creating a references to shoppinglists collection
+     this.referenceMessages = firebase
+     .firestore()
+     .collection("messages");
+   
     
     let name = this.props.route.params.name;
     let color = this.props.route.params.color;
     this.props.navigation.setOptions({ title: name, backgroundColor: color });
+
+// listen to authentication events
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user?.uid,
+        messages: [],
+      });
+      this.unsubscribe = this.referenceMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
   };
 
   onSend(messages = []) {
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    }),
+    () => {
+      this.addMessage();
+    }
+    );
   };
+
+  componentWillUnmount() {
+    // stop listening to authentication
+    this.authUnsubscribe();
+    // stop listening for changes
+    this.unsubscribeListUser();
+  }
 
   renderBubble(props) {
     return (
